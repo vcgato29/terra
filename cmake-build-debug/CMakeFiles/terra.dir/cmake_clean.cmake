fff1a8aed4ddfcdee083b4df21eefb4adf92a517
nixxholas <nicholas@slyck.net>
2017-12-31 14:07:57 +0800

    Source: Initial Commit
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ cmake-build-debug/CMakeFiles/terra.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/terra.dir/main.cpp.o"
+  "terra.pdb"
+  "terra"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/terra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "command line arguments:\n";
+    for (int i(1); i != argc; ++i)
+        std::cout << i << "=" << argv[i] << "\n";
+    return 0;
+}
